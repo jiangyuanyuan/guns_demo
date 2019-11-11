aea591e817a21f6e2ca3503750b9b76b4ccf4b7e
jiangyuanyuan <jiangyuanyuan@gmail.com>
2019-11-11 22:05:11 +0800

    注册

---
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/user/UserController.java
@@ -0,0 +1,42 @@
+package com.stylefeng.guns.rest.modular.user;
+
+import com.stylefeng.guns.api.user.UserAPI;
+import com.stylefeng.guns.api.user.vo.UserModel;
+import com.stylefeng.guns.rest.modular.vo.RespenseVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * <p>
+ * 用户表 前端控制器
+ * </p>
+ *
+ * @author freedom
+ * @since 2019-11-11
+ */
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserAPI userAPI;
+
+
+    public RespenseVo register(UserModel userModel){
+        if (userModel.getUsername()==null||userModel.getUsername().trim().length()==0){
+            return RespenseVo.fail("用户名不能为空");
+        }
+        if (userModel.getPassword()==null||userModel.getPassword().trim().length()==0){
+            return RespenseVo.fail("密码不能为空");
+        }
+
+        boolean isSuccess = userAPI.register(userModel);
+        if (isSuccess){
+            return RespenseVo.ok("注册成功");
+        }else {
+            return RespenseVo.fail("注册失败");
+        }
+
+
+    }
+}
---
+++ guns-user/src/main/java/TTT/IMoocUserTService.java
@@ -0,0 +1,16 @@
+package TTT;
+
+import com.stylefeng.guns.rest.common.persistence.model.MoocUserT;
+import com.baomidou.mybatisplus.service.IService;
+
+/**
+ * <p>
+ * 用户表 服务类
+ * </p>
+ *
+ * @author freedom
+ * @since 2019-11-11
+ */
+public interface IMoocUserTService extends IService<MoocUserT> {
+
+}
